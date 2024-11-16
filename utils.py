f9f7eac8832b6a93f5f1d6c033c2c9611b758bfe
Harsh Benahalkar <benahalkar@gmail.com>
2024-11-16 14:04:34 -0500

    shifting profile time to class

--- utils.py
+++ utils.py
@@ -2,35 +2,37 @@ import time
 from constants import VERBOSE_CODE
 
 
-__profile_times, __profile_messages = [], []
-__lasttime, __currtime = time.monotonic(), time.monotonic()
+class TimeProfiler():
+    def __init__(self):
+        self._profile_times, self._profile_messages = [], []
+        self._lasttime, self._currtime = time.monotonic(), time.monotonic()
 
-def profile_time(message):
-    global __lasttime, __currtime, __profile_times, __profile_messages
 
-    if message.lower() == "start":
-        __lasttime, __currtime = time.monotonic(), time.monotonic()
+    def profile_time(self, message):
 
-    elif message.lower() == "stop":
-        __lasttime, __currtime = time.monotonic(), time.monotonic()
+        if message.lower() == "start":
+            self._lasttime, self._currtime = time.monotonic(), time.monotonic()
 
-        assert len(__profile_times) == len(__profile_messages), "Somethings wrong with profile times"
+        elif message.lower() == "stop":
+            self._lasttime, self._currtime = time.monotonic(), time.monotonic()
 
-        __profile_messages = list(map(lambda x : "`" + str(x) + "`", __profile_messages))
+            assert len(self._profile_times) == len(self._profile_messages), "Somethings wrong with profile times"
 
-        max_length = len(max(__profile_messages, key=len))
-        total_time = sum(__profile_times)
+            self._profile_messages = list(map(lambda x : "`" + str(x) + "`", self._profile_messages))
 
-        if VERBOSE_CODE:
-            print("\n" + "="*80, "\nPROFILING TIMES")
-            for _time, _msg in zip(__profile_times, __profile_messages): 
-                print(f"Time for {_msg.ljust(max_length)}:", f"{_time:08.5f}", "s", "//", f"{((_time*100)/total_time):05.2f}", "%")
-            print("="*80)
-        __profile_times, __profile_messages = [], []
+            max_length = len(max(self._profile_messages, key=len))
+            total_time = sum(self._profile_times)
 
-    else:
-        __profile_messages.append(message)
-        __currtime = time.monotonic()
-        __profile_times.append(__currtime - __lasttime)
+            if VERBOSE_CODE:
+                print("\n" + "="*80, "\nPROFILING TIMES")
+                for _time, _msg in zip(self._profile_times, self._profile_messages): 
+                    print(f"Time for {_msg.ljust(max_length)}:", f"{_time:08.5f}", "s", "//", f"{((_time*100)/total_time):05.2f}", "%")
+                print("="*80)
+            self._profile_times, self._profile_messages = [], []
 
-        __lasttime = __currtime
+        else:
+            self._profile_messages.append(message)
+            self._currtime = time.monotonic()
+            self._profile_times.append(self._currtime - self._lasttime)
+
+            self._lasttime = self._currtime
