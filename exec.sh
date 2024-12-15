aab4310c5878896608dc87ff938cad99321328ac
sidijju <sidijju@gmail.com>
2024-12-15 16:10:25 -0500

    refactored .sh scripts and removed encoder folder

--- exec.sh
+++ exec.sh
@@ -1,5 +1,4 @@
 # Check difference between our code and original code (for nested tensor vs without)
-# Comparison check to see if our code matches the original code output
 
 start=$(date +%s)
 
@@ -8,27 +7,30 @@ PYTHON_PATH=$BASEDIR/.venv_nested/bin/python
 MAINFILE=$BASEDIR/main.py
 EVALFILE=$BASEDIR/eval.py
 
-DATA_PATH=$BASEDIR"/data"
-VANILLA_PATH=$DATA_PATH"/unnested_tensor.pt"
-NESTED_PATH=$DATA_PATH"/nested_tensor.pt"
+DATA_PATH = $BASEDIR"/comp-data"
+VANILLA_PATH = $DATA_PATH"/unnested_tensor.pt"
+NESTED_PATH = $DATA_PATH"/nested_tensor.pt"
 
 # Remove old data
 
-rm -rf $DATA_PATH
+rm $VANILLA_PATH
+rm $NESTED_PATH
 
 # Test without nested tensors
 
 export LOGFILE=$VANILLA_PATH
-$PYTHON_PATH $MAINFILE
+$PYTHON_PATH $MAINFILE 
 
 # Test with nested tensors
+# Test with nested tensors
 
+export LOGFILE=$NESTED_PATH
 export LOGFILE=$NESTED_PATH
 $PYTHON_PATH $MAINFILE --nest_tensor
 
 # Run evaluation
 
-$PYTHON_PATH $EVALFILE $NESTED_PATH $VANILLA_PATH 
+$PYTHON_PATH $EVALFILE $VANILLA_PATH $NESTED_PATH
 
 end=$(date +%s)
 echo "Elapsed Time: $((end-start)) seconds"
