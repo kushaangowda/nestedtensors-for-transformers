693f051bc4e0546aa4318dceeb0956fd7fdce0a9
Harsh Benahalkar <benahalkar@gmail.com>
2024-12-17 21:26:46 -0500

    rough book

--- test.py
+++ test.py
@@ -1,14 +1,10 @@
 import torch
 
-tensor = torch.rand(4,3,6)
 
-a, b = torch.split(tensor, [3,3], dim=2)
+tensor = torch.nested.as_nested_tensor([torch.rand(4,6), torch.rand(3,6), torch.rand(1,6), torch.rand(5,6)])
 
-print(tensor.shape, a.shape, b.shape)
+print(tensor)
 
+print("\\")
 
-tensor = torch.nested.nested_tensor([torch.rand(4,6), torch.rand(3,6), torch.rand(1,6), torch.rand(5,6)])
-
-a, b = torch.split(tensor, [3,3], dim=2)
-
-print([ten.shape for ten in tensor.unbind(0)], "\n", [ten.shape for ten in a.unbind(0)], "\n", [ten.shape for ten in b.unbind(0)])
+print(tensor + tensor)
